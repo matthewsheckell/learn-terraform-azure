676bc21bbb16afe7357da908faf53d07173b93ee
Matthew Sheckell <matthew.sheckell@outlook.com>
2023-09-17 15:52:23 -0400

    Added tags to rg in main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,21 @@ provider "azurerm" {
   features {}
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
 }
