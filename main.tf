f071e99d86e8871aa702081cb85e035b2c3f6826
Matthew Sheckell <matthew.sheckell@outlook.com>
2023-09-17 16:05:32 -0400

    Created variables.tf. Added rg variable.

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = var.resource_group_name
   location = "eastus"
 
   tags = {
